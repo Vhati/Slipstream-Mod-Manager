Vhati/Slipstream-Mod-Manager

commit 975fe00071b9a9797eaf3b39ac687c605399873a

Added rebuildXMLFile() to repair non-append xml in mods during patching

src/main/java/net/vhati/modmanager/core/ModPatchThread.java
@@ -237,16 +237,27 @@ public class ModPatchThread extends Thread {
 									moddedItems.add( innerPath );
 							}
 						}
-						else if ( fileName.endsWith( ".xml" ) || fileName.endsWith( ".txt" ) ) {
+						if ( fileName.endsWith( ".xml" ) ) {
+							innerPath = checkCase( innerPath, knownPaths, knownPathsLower );
+
+							InputStream fixedStream = ModUtilities.rebuildXMLFile( zis, "windows-1252", modFile.getName()+":"+parentPath+fileName );
+
+							if ( !moddedItems.contains(innerPath) )
+								moddedItems.add( innerPath );
+							else
+								log.warn( String.format( "Clobbering earlier mods: %s", innerPath ) );
+
+							if ( ftlP.contains( innerPath ) )
+								ftlP.remove( innerPath );
+							ftlP.add( innerPath, fixedStream );
+						}
+						else if ( fileName.endsWith( ".txt" ) ) {
 							innerPath = checkCase( innerPath, knownPaths, knownPathsLower );
 
 							// Normalize line endings for other text files to CR-LF.
 							//   decodeText() reads anything and returns an LF string.
 							String fixedText = ModUtilities.decodeText( zis, modFile.getName()+":"+parentPath+fileName ).text;
 							fixedText = Pattern.compile("\n").matcher( fixedText ).replaceAll( "\r\n" );
-
-							if ( fileName.endsWith( ".xml" ) )
-								fixedText = fixedText.replaceAll( "<[?]xml [^>]*?[?]>\n*", "<?xml version=\"1.0\" encoding=\"windows-1252\"?>\n" );
 
 							InputStream fixedStream = ModUtilities.encodeText( fixedText, "windows-1252", modFile.getName()+":"+parentPath+fileName+" (with new EOL)" );
 

src/main/java/net/vhati/modmanager/core/ModUtilities.java
@@ -199,8 +199,8 @@ public class ModUtilities {
 	 * which doesn't need closing.
 	 *
 	 * The result will have CR-LF line endings and the desired encoding.
-	 * FTL stubbornly assumes all XML is in windows-1252 encoding, even
-	 * on Linux.
+	 * Note: FTL stubbornly assumes all XML is in windows-1252 encoding,
+	 * even on Linux.
 	 *
 	 * The description arguments identify the streams for log messages.
 	 *
@@ -228,6 +228,39 @@ public class ModUtilities {
 		String mergedString = writer.toString();
 
 		InputStream result = encodeText( mergedString, encoding, mainDescription+"+"+appendDescription );
+		return result;
+	}
+
+	/**
+	 * Decodes, parses, sloppy prints, and reencodes an XML stream.
+	 * This effectively repairs any XML problems that the sloppy parser
+	 * can tolerate.
+	 *
+	 * The returned stream is a ByteArrayInputStream
+	 * which doesn't need closing.
+	 *
+	 * The result will have CR-LF line endings and the desired encoding.
+	 * Note: FTL stubbornly assumes all XML is in windows-1252 encoding,
+	 * even on Linux.
+	 *
+	 * The description argument identifies the stream for log messages.
+	 *
+	 * @see net.vhati.modmanager.core.XMLPatcher
+	 * @see net.vhati.modmanager.core.SloppyXMLOutputProcessor
+	 */
+	public static InputStream rebuildXMLFile( InputStream srcStream, String encoding, String srcDescription ) throws IOException, JDOMException {
+		Pattern xmlDeclPtn = Pattern.compile( "<[?]xml [^>]*?[?]>\n*" );
+
+		String srcText = decodeText( srcStream, srcDescription ).text;
+		srcText = xmlDeclPtn.matcher(srcText).replaceFirst( "" );
+		srcText = "<wrapper xmlns:mod='mod' xmlns:mod-append='mod-append' xmlns:mod-overwrite='mod-overwrite'>"+ srcText +"</wrapper>";
+		Document doc = parseStrictOrSloppyXML( srcText, srcDescription+" (wrapped)" );
+
+		StringWriter writer = new StringWriter();
+		SloppyXMLOutputProcessor.sloppyPrint( doc, writer, encoding );
+		String resultString = writer.toString();
+
+		InputStream result = encodeText( resultString, encoding, srcDescription+" (cleaned)" );
 		return result;
 	}
 

commit 78704d237740d85ea61e2c9737551e65923c481d

Fixed corruption during patching caused by commit 975fe00

src/main/java/net/vhati/modmanager/core/ModPatchThread.java
@@ -237,7 +237,7 @@ public class ModPatchThread extends Thread {
 									moddedItems.add( innerPath );
 							}
 						}
-						if ( fileName.endsWith( ".xml" ) ) {
+						else if ( fileName.endsWith( ".xml" ) ) {
 							innerPath = checkCase( innerPath, knownPaths, knownPathsLower );
 
 							InputStream fixedStream = ModUtilities.rebuildXMLFile( zis, "windows-1252", modFile.getName()+":"+parentPath+fileName );