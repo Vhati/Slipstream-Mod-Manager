ffc8770d14d1f35b399915dcb83e1da31f6cf089
Vhati <tvtronix@yahoo.com>
2018-01-07 16:02:44 -0500

    Incremented version

--- src/main/java/net/vhati/modmanager/FTLModManager.java
+++ src/main/java/net/vhati/modmanager/FTLModManager.java
@@ -35,7 +35,7 @@ public class FTLModManager {
 	private static final Logger log = LoggerFactory.getLogger( FTLModManager.class );
 
 	public static final String APP_NAME = "Slipstream Mod Manager";
-	public static final ComparableVersion APP_VERSION = new ComparableVersion( "1.9" );
+	public static final ComparableVersion APP_VERSION = new ComparableVersion( "1.9.1" );
 	public static final String APP_URL = "https://subsetgames.com/forum/viewtopic.php?f=12&t=17102";
 	public static final String APP_AUTHOR = "Vhati";
 
